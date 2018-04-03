013ecc72e5a7a061977b4c7b6116767143e5650d
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2018-04-03 10:56:23 +0700

    Add schema: perl::modargs

--- lib/Sah/Schema/perl/modname.pm
+++ lib/Sah/Schema/perl/modname.pm
@@ -5,7 +5,18 @@ package Sah::Schema::perl::modname;
 
 our $schema = [str => {
     summary => 'Perl module name',
+    description => <<'_',
+
+Perl module name, e.g. `Foo`, `Foo::Bar`.
+
+Contains coercion rule so you can also input `Foo-Bar`, `Foo/Bar`, or
+`Foo/Bar.pm` and it will be normalized into `Foo::Bar`.
+
+See also: `perl::modargs`.
+
+_
     match => '\A[A-Za-z_][A-Za-z_0-9]*(::[A-Za-z_0-9]+)*\z',
+
     'x.perl.coerce_rules' => [
         'str_normalize_perl_modname',
     ],
