8e055f61ffa76e1fe2fd42078ace43f64410025c
perlancar (on netbook-zenbook-ux305) <perlancar@gmail.com>
2019-06-03 06:12:47 +0700

    Add schema: perl::podname

---
+++ lib/Sah/Schema/perl/podname.pm
@@ -0,0 +1,27 @@
+package Sah::Schema::perl::podname;
+
+# DATE
+# VERSION
+
+our $schema = ['str' => {
+    summary => 'Perl POD name',
+    description => <<'_',
+
+Perl POD name, e.g. `Config`, `Some::Other::POD`.
+
+Basically the same as `perl::modname`, but with a different completion.
+
+_
+    match => '\A[A-Za-z_][A-Za-z_0-9]*(::[A-Za-z_0-9]+)*\z',
+
+    'x.perl.coerce_rules' => [
+        'str_normalize_perl_modname',
+    ],
+
+    # provide a default completion which is from list of installed perl PODs
+    'x.completion' => 'perl_modname',
+
+}, {}];
+
+1;
+# ABSTRACT:
