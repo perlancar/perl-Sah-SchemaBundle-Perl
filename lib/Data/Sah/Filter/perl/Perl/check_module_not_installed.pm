5b505d6ea42a4545e914b0ab22c7e9cf04f7f8b4
perlancar (on pc-home) <perlancar@gmail.com>
2021-07-19 19:52:15 +0700

    Add schema: perl::modname::not_installed

---
+++ lib/Data/Sah/Filter/perl/Perl/check_module_not_installed.pm
@@ -0,0 +1,39 @@
+package Data::Sah::Filter::perl::Perl::check_module_not_installed;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+sub meta {
+    +{
+        v => 1,
+        summary => 'Check that module is not installed locally',
+        might_fail => 1,
+    };
+}
+
+sub filter {
+    my %args = @_;
+
+    my $dt = $args{data_term};
+
+    my $res = {};
+
+    $res->{modules}{"Module::Installed::Tiny"} //= 0;
+    $res->{expr_filter} = join(
+        "",
+        "do { my \$tmp = $dt; if (Module::Installed::Tiny::module_installed(\$tmp)) { [\"Module \$tmp is installed\", \$tmp] } else { [undef, \$tmp] } }",
+    );
+
+    $res;
+}
+
+1;
+# ABSTRACT:
+
+=for Pod::Coverage ^(meta|filter)$
---
+++ lib/Sah/Schema/perl/modname/not_installed.pm
@@ -0,0 +1,29 @@
+package Sah::Schema::perl::modname::not_installed;
+
+# AUTHORITY
+# DATE
+# DIST
+# VERSION
+
+our $schema = ['perl::modname' => {
+    summary => 'Name of a Perl module that is not installed locally',
+    description => <<'_',
+
+This schema is based on the `perl::modname` schema with an additional check that
+the perl module is not installed locally. Checking is done using
+<pm:Module::Installed::Tiny>. This check fetches the source code of the module
+from filesystem or %INC hooks, but does not actually load/execute the code.
+
+_
+
+    'prefilters' => [
+        'Perl::check_module_not_installed',
+    ],
+
+    examples => [
+    ],
+
+}, {}];
+
+1;
+# ABSTRACT:
