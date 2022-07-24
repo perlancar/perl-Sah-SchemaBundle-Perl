f9ec95169928dab8849cd36020fcaea1af49fb82
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2022-07-24 20:17:21 +0700

    [doc] Add description to perl::distname schema

--- lib/Sah/Schema/perl/distname.pm
+++ lib/Sah/Schema/perl/distname.pm
@@ -20,14 +20,23 @@ our $schema = [str => {
 
     description => <<'_',
 
-For convenience (particularly in CLI with tab completion), you can input one of:
+This is a schema you can use when you want to accept a Perl distribution name,
+e.g. `WWW-Mechanize`. It offers basic checking of syntax as well as a couple of
+conveniences. First, it offers completion from list of locally installed Perl
+distribution. Second, it contains coercion rule so you can also input
+`Foo::Bar`, `Foo/Bar`, `Foo/Bar.pm`, or even 'Foo.Bar' and it will be normalized
+into `Foo-Bar`.
 
-    Foo::Bar
-    Foo/Bar
-    Foo/Bar.pm
-    Foo.Bar
+To see this schema in action on the CLI, you can try e.g. the `dist-has-deb`
+script from <pm:App::DistUtils> and activate its tab completion (see its manpage
+for more details). Then on the CLI try typing:
 
-and it will be coerced into Foo-Bar form.
+    % dist-has-deb WWW-<tab>
+    % dist-has-deb dzp/<tab>
+
+Note that this schema does not check that the Perl disribution exists on CPAN or
+is installed locally. To check that, use the `perl::distname::installed` schema.
+And there's also a `perl::distname::not_installed` schema.
 
 _
 }];
