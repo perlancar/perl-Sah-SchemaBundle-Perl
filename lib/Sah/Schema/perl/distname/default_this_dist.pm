2aec3f3d3a331043fd7e066b71d18f5313896ffe
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2023-10-26 07:58:05 +0700

    [doc][ux] Give some warning when using a schema with default value for destructive actions

--- lib/Sah/Schema/perl/distname/default_this_dist.pm
+++ lib/Sah/Schema/perl/distname/default_this_dist.pm
@@ -16,6 +16,12 @@ See <pm:App::ThisDist>'s `this_dist()` for more details on how "this
 distribution" is determined. Note that `App::ThisDist` is not added as
 dependency automatically; you will have to add it manually.
 
+Note: be careful when using this schema for actions that are destructive to a
+Perl dist or that change some things, because a user can perform those actions
+without giving an argument (e.g. a `delete-dist` script). It is safer to use
+this schema to perform a non=destructive action (e.g. `ls-dist`) and/or
+operate in dry-run mode by default.
+
 _
 
     'x.perl.default_value_rules' => [
--- lib/Sah/Schema/perl/modname/default_this_mod.pm
+++ lib/Sah/Schema/perl/modname/default_this_mod.pm
@@ -16,6 +16,12 @@ See <pm:App::ThisDist>'s `this_mod()` for more details on how "this module" is
 determined. Note that `App::ThisDist` is not added as dependency automatically;
 you will have to add it manually.
 
+Note: be careful when using this schema for actions that are destructive to a
+Perl module or that change some things, because a user can perform those actions
+without giving an argument (e.g. a `delete-module` script). It is safer to use
+this schema to perform a non=destructive action (e.g. `man-module`) and/or
+operate in dry-run mode by default.
+
 _
 
     'x.perl.default_value_rules' => [
